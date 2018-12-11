2c63174d4c69ea823cffc17ae110dc5a74facae6
Kirill Golubev <arnasay@gmail.com>
2018-12-10 22:35:31 -0600

    Add, edit bookmarks features added

--- loged.php
+++ loged.php
@@ -16,6 +16,7 @@
         //get all bookmarks for the this particular user
         $requestBookmarks = "SELECT * FROM bookmarks WHERE userID = \""  .$_SESSION["userID"] . "\"";
         $requestResult = mysqli_query($dbConnection, $requestBookmarks);
+        mysqli_close($dbConnection);
 
         return $requestResult;
     }
@@ -34,8 +35,59 @@
 
 
     //Check if any form button was pressed
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if (isset($_POST["newUrlName"]) && isset($_POST["newUrl"])) {
+            insertNewBookmarkToDatabase();            
+        }
         
+        //Check if remove button was pressed
+        if (isset($_POST["removeBookmark"])) {
+            removeBookmarkFromDatabase($_POST["removeBookmark"]);
+        }
+
+        //Check if user button to update bookmark
+        if (isset($_POST["updateBookmark"])) {
+            echo ("updateBookmark");
+            updateBookmarkInDatabase($_POST["updateBookmark"]);
+            
+        }
+
+        $_POST = array();   
+        unset($_REQUEST);
+    }
+
+    //Insert new bookmarks to database 
+    function insertNewBookmarkToDatabase() {
+        $dbConnection = connectToDB();
+        mysqli_select_db($dbConnection, "bookmarks");
+
+        $insertQuery = "INSERT INTO bookmarks (userID, url, name) VALUES(" 
+            . $_SESSION["userID"] . ", \"" . $_POST["newUrl"] . "\", \"" . $_POST["newUrlName"] . "\")";
+            // echo $insertQuery;
+        $insertResult = mysqli_query($dbConnection, $insertQuery);
+        mysqli_close($dbConnection);
+    }
+
+    //Edit bookmark
+    function updateBookmarkInDatabase($bookmarkID) {
+        $dbConnection = connectToDB();
+        mysqli_select_db($dbConnection, "bookmarks");
+
+        $updateQuery = "UPDATE bookmarks SET url=\"" . $_POST["linkName$bookmarkID"] . 
+            "/, name=\"" . $_POST["linkUrl$bookmarkID"] . "\" WHERE bookmarkID=$bookmarkID" ;
+        mysqli_query($dbConnection, $updateQuery);
+        mysqli_close($dbConnection);
+        echo ("DB closed");
+    }
+
+    //Remove bookmark with specified id from database 
+    function removeBookmarkFromDatabase($bookmarkID) {
+        $dbConnection = connectToDB();
+        mysqli_select_db($dbConnection, "bookmarks");
+
+        $removeQuery = "DELETE FROM bookmarks WHERE bookmarkID = $bookmarkID";
+        $result = mysqli_query($dbConnection, $removeQuery);
+        mysqli_close($dbConnection);
     }
     //=============================================================================//
     //***PHP Code Ends***/   
@@ -49,6 +101,7 @@
     <title>Bookmark iT! Your personal bookmarks storage</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="main.js"></script>
 </head>
 <body>
@@ -69,21 +122,19 @@
             //***PHP Code ***/
 
             $bookmarks = getBookmarks(); //Get user bookmarks from database
-            $bookmarkCounter = 0;
 
             if (mysqli_num_rows(($bookmarks)) > 0) {
-                echo "<h3>You have the following bookmarks:</h3><ul><form id=\"bookmarkForm\" action=\"/loged.php\" method=\"POST\">";
+                echo "<h3>Your bookmarks:</h3><ul><form id=\"bookmarkForm\" action=\"loged.php\" method=\"POST\">";
                 while ($row = mysqli_fetch_assoc($bookmarks)) {
-                    echo ("<li><input natype=\"text\" name=\"linkName$bookmarkCounter\" class=\"bookmarkFiels\" value=\"" . $row["name"] . "\" disabled />");
-                    echo ("<input natype=\"text\" name=\"linkUrl$bookmarkCounter\" class=\"bookmarkFiels urlField\" value=\"" . $row["url"] . "\" disabled /><wbr>");
-                    echo ("<wbr><a class=\"bookmarkButton\" href=\"" . $row["url"] . "\" target=\"_blank\">Open &#8594;</a><wbr>");
-                    echo ("<button class=\"bookmarkButton\" type=\"submit\" name=\"editBookmark$bookmarkCounter\" formaction=\"loged.php\">Remove</button>");
-                    echo ("<button class=\"bookmarkButton\" name=\"editBookmark$bookmarkCounter\" formaction=\"#\">Edit</button><wbr>");
-
-                    $bookmarkCounter++;
+                    echo ("<li><input type=\"text\" name=\"linkName" . $row["bookmarkID"] . "\" class=\"bookmarkFiels\" value=\"" . $row["name"] . "\"  disabled/>");
+                    echo ("<input type=\"text\" name=\"linkUrl" . $row["bookmarkID"] . "\" class=\"bookmarkFiels urlField\" value=\"" . $row["url"] . "\"  disabled />");
+                    echo ("<a class=\"bookmarkButton\" href=\"" . $row["url"] . "\" target=\"_blank\">Open &#8594;</a>");
+                    echo ("<button class=\"bookmarkButton\" type=\"submit\" value=\"" . $row["bookmarkID"] . "\"" . $row["bookmarkID"] . " name=\"removeBookmark\" formaction=\"loged.php\">Remove</button>");
+                    echo ("<button class=\"bookmarkButton editButton\" type=\"submit\" name='updateBookmark' value=\"" . $row["bookmarkID"] . "\" formaction=\"loged.php\">Edit</button>");
+                    echo ("<button class=\"bookmarkButton cancelButton\" value=\"" . $row["bookmarkID"] . "\" formaction=\"#\">Cancel</button>");
                 }
                 echo "</form></ul>";
-                $bookmarkCounter = 0;
+
             }
             else {
                 echo "<h3>You don't have any bookmarks, but you can add some</h3>";
--- main.js
+++ main.js
@@ -1,72 +1,132 @@
-let websiteInputForm, addButton, addBookmark, bookmarkForm;
+let newUrl, addButton, editButtons, cancelButtons, addBookmark, bookmarkFiels, bookmarkForm;
+let addElement;    
 
 //Method called when page is loaded for the first time
 const initialize = () => {
-  websiteInputForm = document.getElementById("websiteInputForm");
-  // addButton = document.getElementById("addButton");
+  addElement = document.createElement("li");
+  addElement.innerHTML = "<input type='text' name='newUrlName' class='bookmarkFiels' id='newUrlName' placeholder='Website name' autocomplete='off'/>" +
+  "<input type='text' name='newUrl' class='bookmarkFiels urlField' id='newUrl' placeholder='Website URL'/ autocomplete='off'>" + 
+  "<button class='bookmarkButton' formaction='loged.php' id='addButton'>Add</button>";  
+
   addBookmark = document.getElementById("addBookmark");
   bookmarkForm = document.getElementById("bookmarkForm");
+  bookmarkFiels = document.getElementsByClassName("bookmarkFiels");
+  editButtons = document.getElementsByClassName("editButton");
+  cancelButtons = document.getElementsByClassName("cancelButton");
+
   //Add event listeners to objects
-  // addButton.addEventListener("click", addWebsite, false);
   addBookmark.addEventListener("click", showAddFields, true);
+  
+  //Add event listeners to edit buttons
+  for (let i = 0; i < editButtons.length; i++) {
+    editButtons[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      
+      if (event.currentTarget.innerHTML === "Edit") {
+        event.currentTarget.innerHTML = "Save";
+        $(event.currentTarget).next().show();
+        $(event.currentTarget).prev().prev().hide();
+
+        $(event.currentTarget).siblings(".bookmarkFiels").prop("disabled", false);
+        $(event.currentTarget).siblings(":first").focus();
+      }
+      else {        
+        event.currentTarget.innerHTML = "Edit";
+        $(event.currentTarget).next().hide();
+        $(event.currentTarget).prev().prev().show();
+
+        $(event.currentTarget).siblings(".bookmarkFiels").prop("disabled", true);
+        $(event.currentTarget).unbind('submit').submit();    
+      }     
+    }, false);
+  }
+
+  //Add event listeners to cancel buttons
+  for (let i = 0; i < cancelButtons.length; i++) {
+    cancelButtons[i].addEventListener("click", function(event) {
+      event.preventDefault();
+
+      $(event.currentTarget).prev().html("Edit");
+      $(event.currentTarget).siblings("a").show();   
+      $(event.currentTarget).siblings(".bookmarkFiels").prop("disabled", true);
+      $(event.currentTarget).hide();
+  
+    }, false);
+  }
 };
 
 const showAddFields = () => {
+  
   if (addBookmark.innerHTML === "Add new bookmark") {
-    addBookmark.innerHTML = "Close form";
-    bookmarkForm.innerHTML += "<li><input natype='text' name='linkName$bookmarkCounter' class='bookmarkFiels' placeholder='Name'/>" +
-      "<input natype='text' class='bookmarkFiels urlField' placeholder='URL'/>" + 
-      "<button class='bookmarkButton' formaction='#'>Add</button><wbr></li>";
-  }
-  else {
+    addBookmark.innerHTML = "Complete adding";
     
-    addBookmark.innerHTML = "Add new bookmark";
-  }
+    bookmarkForm.appendChild(addElement);
+    addButton = document.getElementById("addButton");
+    addButton.addEventListener("click", addWebsite, false);
 
+    newUrl = document.getElementById("newUrl");
+    newUrl.addEventListener("focus", () => {
+      if (newUrl.value === "") {
+        newUrl.value = "http://www.";
+      }
+    }, false);
+    newUrl.addEventListener("blur", () => {
+      if (newUrl.value === "http://www.") {
+        newUrl.value = "";
+      }
+    }, false);
+  }
+  else {    
+    addBookmark.innerHTML = "Add new bookmark";
+    bookmarkForm.removeChild(addElement);
+  }
 }
 
+//If add button was pressed 
+const addWebsite = async (event) => {
+  event.preventDefault();
+  if (newUrlName.value === "") {
+    window.alert("Url name cannot be empty!");
+    return;
+  }
 
+  if (newUrl.value.indexOf('.') < 0) { //Check if user input contains at least one dot, otherwise the web address is a priori invalid
+    window.alert("Please enter valid web address!");
+  }
+  else {
+    let isWebsiteValid = await checkWebsiteValidity(newUrl.value);
 
+    if (isWebsiteValid) {
+      alert (`${newUrl.value} is valid and added to bookmarks`);
+      
+      bookmarkForm.submit();
+      newUrlName.value = ""
+      newUrl.value = "";
 
+    }
+    else {
+      window.alert("The entered web address is not active! Please check your input");
+    }    
+  }
+};
 
+//Method checking website validity 
+const checkWebsiteValidity = async (url) => {
+  try {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
 
-
-// //If add button was pressed 
-// const addWebsite = async () => {
-//   if (websiteInputForm.value.indexOf('.') < 0) { //Check if user input contains at least one dot, otherwise the web address is a priori invalid
-//     window.alert("Please enter valid web address!");
-//     websiteInputForm.value = "";
-//   }
-//   else {
-//     let isWebsiteValid = await checkWebsiteValidity(websiteInputForm.value);
-
-//     if (isWebsiteValid) {
-//       alert (`${websiteInputForm.value} is valid and added to bookmarks`);
-//     }
-//     else {
-//       window.alert("The entered web address is not active! Please check your input");
-//       websiteInputForm.value = "";
-//     }    
-//   }
-// };
-
-// //Method checking website validity 
-// const checkWebsiteValidity = async (url) => {
-//   try {
-//     const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
-
-//     if (response.ok) {
-//       console.log(response);
-//       return true;
-//     }
-//     else {
-//       return false;
-//     }
-//   }
-//   catch (networkError) {
-//     console.log(networkError);
-//     return false;
-//   }
-// };
+    if (response.ok) {
+      console.log(response);
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  catch (networkError) {
+    console.log(networkError);
+    return false;
+  }
+};
 
 window.addEventListener("load", initialize, false);
